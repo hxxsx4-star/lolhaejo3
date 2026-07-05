f38da7632649a878afb238f4426706fac2a49acd
hxxsx4-star <bjyy1@naver.com>
2026-07-06 01:12:42 +0900

    배틀기능, 상점기능 추가. UI 개선 포인트 연동

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ class LegendBot(commands.Bot):
             'cogs.petsystem.cog_admin',       # 관리자 명령어 시스템
             'cogs.petsystem.shop',                   # ✨ 펫 상점 시스템 추가
             'cogs.petsystem.battle'                     # ✨ 배틀 시스템 추가
-        ]
+            'cogs.petsystem.debug']
 
         for extension in extensions:
             try:
