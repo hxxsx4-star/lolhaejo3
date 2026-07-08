1063bf7213525a94b8601e08156c7bfe796d1311
hxxsx4-star <bjyy1@naver.com>
2026-07-08 13:52:14 +0900

    대규모 개편

--- main.py
+++ main.py
@@ -33,7 +33,8 @@ class LegendBot(commands.Bot):
             'cogs.petsystem.achievement',     # ✨ 신규 업적 시스템
             'cogs.petsystem.cog_synthetis',   # 50회 업적 연동 합성
             'cogs.petsystem.cog_box',          # 기존 박스 (유지)
-            'cogs.predict'
+            'cogs.predict',
+            'cogs.ui_predict.py'
         ]
 
         for extension in extensions:
